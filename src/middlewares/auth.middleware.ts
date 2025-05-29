41c897c2c96d1f405d0d2c5e5a34ea8a7419bfab
EscThod <tanarat.dev18@gmail.com>
2025-05-29 12:57:17 +0700

    middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,19 +1,21 @@
-// export const authMiddleware = (app) =>
-//   app.onBeforeHandle(async ({ headers, jwt, set }) => {
-//     const authHeader = headers["authorization"];
+export const authMiddleware = (app) =>
+  app.derive(async ({ headers, jwt, set }) => {
+    const authHeader = headers["authorization"];
 
-//     if (!authHeader) {
-//       set.status = 401;
-//       return { error: "Unauthorized" };
-//     }
+    if (!authHeader) {
+      set.status = 401;
+      throw new Error("Unauthorized");
+    }
 
-//     const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
 
-//     try {
-//       const payload = await jwt.verify(token);
-//       return { user: payload };
-//     } catch {
-//       set.status = 401;
-//       return { error: "Invalid token" };
-//     }
-//   });
+    try {
+      const payload = await jwt.verify(token);
+      return {
+        user: payload,
+      };
+    } catch {
+      set.status = 401;
+      throw new Error("Invalid token");
+    }
+  });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,15 +2,11 @@ import { Elysia } from "elysia";
 import userRoutes from "./user.route";
 import TeacherRoutes from "./teacher.route";
 import ResumeRoute from "./resume.route";
-// import { authMiddleware } from "../middlewares/auth.middleware";
+import { authMiddleware } from "../middlewares/auth.middleware";
 const routes = new Elysia();
 
 routes.group("/api", (app) =>
-  app
-    .use(userRoutes)
-    // .use(authMiddleware)
-    .use(TeacherRoutes)
-    .use(ResumeRoute)
+  app.use(userRoutes).use(authMiddleware).use(TeacherRoutes).use(ResumeRoute)
 );
 
 export default routes;
