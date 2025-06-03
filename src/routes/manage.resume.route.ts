a7fe701e90bd8a6ba2e6cc25986e3e821daaf4ec
EscThod <tanarat.dev18@gmail.com>
2025-06-03 21:18:18 +0700

    group api route

--- src/routes/manage.resume.route.ts
+++ src/routes/manage.resume.route.ts
@@ -9,33 +9,31 @@ import {
 } from "../schema/sql.schema";
 import { ResumeController } from "../controllers/resume.controller";
 export const manageResumeRoutes = (app: Elysia) =>
-  app
+  app.group("/resume", (app) =>
+    app
 
-    .get("/resume", ResumeController.getResume)
-    .get("/resume/:resume_id", ResumeController.getResumeById)
-    .put("/resume/:resume_id/soft-skill", ResumeController.addSoftSkill, {
-      body: addSkillSchema,
-    })
-    .put(
-      "/resume/:resume_id/education-history",
-      ResumeController.addEducationHistory,
-      {
-        body: addEducationHistorySchema,
-      }
-    )
-    .put(
-      "/resume/:resume_id/work-experience",
-      ResumeController.addWorkExperience,
-      {
+      .get("", ResumeController.getResume)
+      .get("/:resume_id", ResumeController.getResumeById)
+      .put("/:resume_id/soft-skill", ResumeController.addSoftSkill, {
+        body: addSkillSchema,
+      })
+      .put(
+        "/:resume_id/education-history",
+        ResumeController.addEducationHistory,
+        {
+          body: addEducationHistorySchema,
+        }
+      )
+      .put("/:resume_id/work-experience", ResumeController.addWorkExperience, {
         body: addWork_historySchema,
-      }
-    )
-    .put("/resume/:resume_id/internship", ResumeController.addinternship, {
-      body: addinternshipSchema,
-    })
-    .put("/resume/:resume_id/project", ResumeController.addproject, {
-      body: addProjectSchema,
-    })
-    .put("/resume/:resume_id/training", ResumeController.addTraning, {
-      body: addTraningSchema,
-    });
+      })
+      .put("/:resume_id/internship", ResumeController.addinternship, {
+        body: addinternshipSchema,
+      })
+      .put("/:resume_id/project", ResumeController.addproject, {
+        body: addProjectSchema,
+      })
+      .put("/:resume_id/training", ResumeController.addTraning, {
+        body: addTraningSchema,
+      })
+  );
--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -2,9 +2,11 @@ import { Elysia } from "elysia";
 import { bodyCreateStudent } from "../schema/sql.schema";
 import { studentController } from "../controllers/student.controller";
 export const studentRoutes = (app: Elysia) =>
-  app
-    .get("/student", studentController.getStudents)
-    .get("/student/:student_id", studentController.getStudentById)
-    .post("/student", studentController.createStudent, {
-      body: bodyCreateStudent,
-    });
+  app.group("/student", (app) =>
+    app
+      .get("", studentController.getStudents)
+      .get("/:student_id", studentController.getStudentById)
+      .post("", studentController.createStudent, {
+        body: bodyCreateStudent,
+      })
+  );
