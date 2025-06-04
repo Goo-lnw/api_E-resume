7113bdcca3e34e9adcea2dfd97c5ef72e7b271ac
Dekpretaa <dekpretaa@gmail.com>
2025-06-04 19:48:42 +0700

    fix delete/edit stud&teach

--- src/routes/teacher.route.ts
+++ src/routes/teacher.route.ts
@@ -1,7 +1,16 @@
 import { Elysia } from "elysia";
-import { bodyCreateStudent } from "../schema/sql.schema";
+import { bodyCreateTeacher } from "../schema/sql.schema";
 import { teacherController } from "../controllers/teacher.controller";
 export const teacherRoutes = (app: Elysia) =>
   app.group("/teacher", (app) =>
-    app.get("", teacherController.testTeacherController)
+    app
+      .get("", teacherController.getTeachers)
+      .get("/:student_id", teacherController.getTeacherById)
+      .delete("/:teacher_id/delete", teacherController.deleteTeacherController)
+      .put("/:teacher_id/edit", teacherController.editTeacherController)
+      .get("/test", teacherController.testTeacher)
+      .post("", teacherController.createTeacher, {
+        body: bodyCreateTeacher,
+      })
+
   );
