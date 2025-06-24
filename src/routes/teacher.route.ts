92291c3b7ce98bb20a8beba7f751b5fa5a70fdbf
TanaratSudjai <tanarat.dev18@gmail.com>
2025-06-24 20:25:49 +0700

    deploy

--- src/routes/teacher.route.ts
+++ src/routes/teacher.route.ts
@@ -1,48 +1,68 @@
 import { Elysia } from "elysia";
 import {
-    activitySchema,
-    activityAssignSchema,
-    bodyCreateTeacher,
-    deleteActivityOfStudentSchema,
+  activitySchema,
+  activityAssignSchema,
+  bodyCreateTeacher,
+  deleteActivityOfStudentSchema,
 } from "../schema/sql.schema";
 import { teacherController } from "../controllers/teacher.controller";
 
 export const teacherRoutes = (app: Elysia) =>
-    app.group("/teacher", (app) =>
-        app
-            .get("", teacherController.getTeachers)
-            .get("/:student_id", teacherController.getTeacherById)
-            .delete("/:teacher_id/delete", teacherController.deleteTeacherController)
-            .put("/:teacher_id/edit", teacherController.editTeacherController)
-            .post("", teacherController.createTeacher, {
-                body: bodyCreateTeacher,
-            })
+  app.group("/teacher", (app) =>
+    app
+      .get("", teacherController.getTeachers)
+      .get("/:student_id", teacherController.getTeacherById)
+      .delete("/:teacher_id/delete", teacherController.deleteTeacherController)
+      .put("/:teacher_id/edit", teacherController.editTeacherController)
+      .post("", teacherController.createTeacher, {
+        body: bodyCreateTeacher,
+      })
 
-            .get("/activity", teacherController.getAllActivity)
-            .get("/activity/:activity_id", teacherController.getActivityById)
-            .get("/activity/student_not_in/:activity_id", teacherController.getStudentByNoActivityId)
-            .get("/activity/student_in/:activity_id", teacherController.getStudentByActivityId)
-            .get("/activity/student_no_cert/:activity_id", teacherController.getStudentActivityCertNotExist)
-            .get("/activity/student_cert/:activity_id", teacherController.getStudentActivityCertExist)
-            .post("/activity", teacherController.createActivity, {
-                body: activitySchema,
-                type: "multipart/form-data",
-            })
-            .post("/activity/check_in", teacherController.checkInActivity, {
-                body: activityAssignSchema,
-            })
-            .post("/activity/assign_cert", teacherController.assignActivityCert, {
-                body: activityAssignSchema,
-            })
-            .put("/activity/:activity_id", teacherController.editActivity, {
-                body: activitySchema,
-                type: "multipart/form-data",
-            })
-            .delete("/activity/:activity_id", teacherController.deleteActivity)
-            .delete("/activity/student/:activity_id", teacherController.deleteActivityStudentCheckin, {
-                body: deleteActivityOfStudentSchema,
-            })
-            .delete("/activity/student_cert/:activity_id", teacherController.deleteStudentActivityCert, {
-                body: deleteActivityOfStudentSchema,
-            })
-    );
+      .get("/activity", teacherController.getAllActivity)
+      .get("/activity/:activity_id", teacherController.getActivityById)
+      .get(
+        "/activity/student_not_in/:activity_id",
+        teacherController.getStudentByNoActivityId
+      )
+      .get(
+        "/activity/student_in/:activity_id",
+        teacherController.getStudentByActivityId
+      )
+      .get(
+        "/activity/student_no_cert/:activity_id",
+        teacherController.getStudentActivityCertNotExist
+      )
+      .get(
+        "/activity/student_cert/:activity_id",
+        teacherController.getStudentActivityCertExist
+      )
+      .post("/activity", teacherController.createActivity, {
+        body: activitySchema,
+        type: "multipart/form-data",
+      })
+      .post("/activity/check_in", teacherController.checkInActivity, {
+        body: activityAssignSchema,
+      })
+      .post("/activity/assign_cert", teacherController.assignActivityCert, {
+        body: activityAssignSchema,
+      })
+      .put("/activity/:activity_id", teacherController.editActivity, {
+        body: activitySchema,
+        type: "multipart/form-data",
+      })
+      .delete("/activity/:activity_id", teacherController.deleteActivity)
+      .delete(
+        "/activity/student/:activity_id",
+        teacherController.deleteActivityStudentCheckin,
+        {
+          body: deleteActivityOfStudentSchema,
+        }
+      )
+      .delete(
+        "/activity/student_cert/:activity_id",
+        teacherController.deleteStudentActivityCert,
+        {
+          body: deleteActivityOfStudentSchema,
+        }
+      )
+  );
