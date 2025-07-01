b183cf208d9862b2f450a1df68a9f8d2576b31cd
TanaratSudjai <tanarat.dev18@gmail.com>
2025-07-02 02:10:02 +0700

    deployed

--- src/middlewares/ratelimiter.middleware.ts
+++ src/middlewares/ratelimiter.middleware.ts
@@ -1,13 +1,13 @@
 import { rateLimit } from "elysia-rate-limit";
 
 export const rateLimitMiddleware = rateLimit({
-    duration: 60000,
-    max: 60,
-    errorResponse: new Response("too many request, please slow down :]", {
-        status: 429,
-        headers: new Headers({
-            "Content-Type": "text/plain",
-            "Custom-Header": "custom",
-        }),
+  duration: 60000,
+  max: 60,
+  errorResponse: new Response("too many request, please slow down :]", {
+    status: 429,
+    headers: new Headers({
+      "Content-Type": "text/plain",
+      "Custom-Header": "custom",
     }),
+  }),
 });
