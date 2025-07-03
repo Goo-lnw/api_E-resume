954438fd412c0fe527e522b0d627256998aa5f21
TanaratSudjai <tanarat.dev18@gmail.com>
2025-07-04 02:02:06 +0700

    deployed

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -12,7 +12,13 @@ export const studentRoutes = (app: Elysia) =>
       .put("/:student_id/edit", studentController.editStudentController)
       .delete("/:student_id/delete", studentController.deleteStudentController)
 
-      .post("/edit_profile", UserController.editProfile)
+      .post(
+        "/edit_profile",
+        {
+          maxRequestBodySize: 20 * 1024 * 1024, 
+        },
+        UserController.editProfile
+      )
 
       .get("/protected", UserController.getStudentSession)
 
